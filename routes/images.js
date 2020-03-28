d0c4ded4cccc51136805fe9e990a7abfe7be2943
Jeremy Dennen <jcdennen@syr.edu>
2020-03-28 13:51:35 -0400

    initial commit with basic Express.js app

---
+++ routes/images.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET images list */
+router.get('/', function(req, res, next) {
+  res.send('TODO');
+});
+
+/* POST new image */
+router.get('/', function(req, res, next) {
+  res.send('TODO');
+});
+
+module.exports = router;
