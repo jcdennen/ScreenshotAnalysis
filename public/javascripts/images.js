b5050901ebccb6a01618f5412e5f724340b5afed
Jeremy Dennen <jcdennen@syr.edu>
2020-03-29 18:58:36 -0400

    more initial work to offer an example for users to interact with

--- public/javascripts/images.js
+++ public/javascripts/images.js
@@ -1,1 +1,91 @@
-console.log("TODO: hook into browser to offer image upload and show progress");
+/**
+* NOTE: this is largely based on an example provided by LogRocket, and serves as a learning tool
+* https://blog.logrocket.com/how-to-extract-text-from-an-image-using-javascript-8fe282fb0e71/
+*/
+const recognitionImageElement = document.querySelector('#recognition-image-input');
+const recognitionConfidenceElement = document.querySelector('#recognition-confidence-input');
+const recognitionProgressElement = document.querySelector('#recognition-progress');
+const recognitionTextElement = document.querySelector('#recognition-text');
+const labeledImageElement = document.querySelector('#labeled-image');
+
+const setImageSrc = (image, imageFile) => {
+    return new Promise((resolve, reject) => {
+        const fr = new FileReader();
+        fr.onload = function() {
+            if (typeof fr.result !== 'string') {
+                return reject(null);
+            }
+            image.src = fr.result;
+            return resolve();
+        };
+        fr.onerror = reject;
+        fr.readAsDataURL(imageFile);
+    });
+};
+
+recognitionImageElement.addEventListener('change', () => {
+    if (!recognitionImageElement.files) {
+        return null;
+    }
+    const file = recognitionImageElement.files[0];
+
+// use Tesseracts Workers
+const worker = Tesseract.createWorker({
+    logger: ({ progress, status }) => {
+        if (!progress || !status || status !== 'recognizing text') {
+            return null;
+        }
+        const p = (progress * 100).toFixed(2);
+        recognitionProgressElement.textContent = `${status}: ${p}%`;
+        recognitionProgressElement.value = p;
+    }
+});
+
+(async () => {
+    await worker.load();
+    await worker.loadLanguage('eng');
+    await worker.initialize('eng');
+    const { data } = await worker.recognize(file);
+
+    await worker.terminate();
+
+    // I'm more interested in pulling out the lines and performing some analysis on them (more to come)
+    const lines = data.lines.filter((line) => {
+        return line.confidence >= 85; // generally, anything less than 85 is bad
+    });
+    console.log((lines.length ? lines :  "couldn't find anything with enough confidence"));
+
+    // The rest is related to the example this is built off of
+    const labeledImage = document.createElement('img');
+    await setImageSrc(labeledImage, file);
+
+    const paragraphsElements = data.paragraphs.map(({ text }) => {
+        const p = document.createElement('p');
+        p.textContent = text;
+        return p;
+    });
+    recognitionTextElement.append(...paragraphsElements);
+
+    const wordsElements = data.words
+        .filter(({ confidence }) => {
+            return confidence > parseInt(recognitionConfidenceElement.value, 10);
+        })
+        .map((word) => {
+            const div = document.createElement('div');
+            const { x0, x1, y0, y1 } = word.bbox;
+            div.classList.add('word-element');
+            Object.assign(div.style, {
+                top: `${y0}px`,
+                left: `${x0}px`,
+                width: `${x1 - x0}px`,
+                height: `${y1 - y0}px`,
+                border: '1px solid black',
+                position: 'absolute',
+            });
+            return div;
+        });
+
+    labeledImageElement.appendChild(labeledImage);
+    labeledImageElement.append(...wordsElements);
+    })();
+});
--- routes/images.js
+++ routes/images.js
@@ -1,50 +1,56 @@
 const express = require('express');
 const router = express.Router();
 const { createWorker } = require('tesseract.js');
-const filename = './public/images/example.jpg'
 
-/* GET image base page */
-router.get('/', function(req, res, next) {
-  res.render('images', { title: 'Image Uploader: In Progress' });
+/* GET image base page - a way for users to test with their own image on the front end */
+router.get('/', (req, res, next) => {
+	res.render('images', { title: 'Image Uploader (WIP)' });
 });
 
-/* GET example screenshot */
-router.get('/example', function(req, res, next) {
+/* GET example screenshot results in JSON*/
+router.get('/json-example', (req, res, next) => {
 
-	// worker without logging
+	// create our worker
 	const worker = createWorker();
 
-	// woker with logging
+	// an example image we'll use to pull text from
+	const filename = './public/images/example.jpg'
+
+	// Example: woker with logging/progress updates (this is too noisy)
 	// const worker = createWorker({
 	//   logger: x => console.log(x),
 	// });
 
 	// Process and return response
+	// there are definitely some better ways of handling the response, but we'll get there in time
+	// I'll likely offload this to the front end, then display the results
 	(async () => {
-	  await worker.load();
-	  await worker.loadLanguage('eng');
-	  await worker.initialize('eng');
-	  const { data } = await worker.recognize(filename);
+		await worker.load();
+		await worker.loadLanguage('eng');
+		await worker.initialize('eng');
+		const { data } = await worker.recognize(filename);
 
-	  // respond as JSON (for now)
-	  // there are definitely some better ways of handling the response, but we'll get there in time
-	  // will likely offload this to the front end, then same the results
-	  res.send(data.lines.filter(function(line) {
-	  	return line.confidence >= 85.0; // anything less than this isn't relavant
+		res.send(data.lines.filter((line) => {
+	  	return line.confidence >= 85.0; // 85 really is that perfect number ( so far ...)
 	  }));
-	  await worker.terminate();
+		await worker.terminate();
 	})();
 });
 
 /**
- * POST new image (TODO)
+ * POST new image
+ * - would be nice for other people to upload their own image and try it
  * - process image
  * - save image file
  * - save results as JSON (for later use)
-*/
-router.post('/upload', function(req, res, next) {
+ */
+ router.post('/upload', (req, res, next) => {
+ 	res.send('TODO');
+ });
 
-  res.send('TODO');
-});
+ // ayye I'm workin on it alright?
+ router.get('/upload', (req, res, next) => {
+ 	res.send('TODO');
+ });
 
-module.exports = router;
+ module.exports = router;
